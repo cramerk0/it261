1c329e1ca4db6f6cc51268c5d4baf7276564d28c
Xiaoyan <122489131+cramerk0@users.noreply.github.com>
2023-02-17 19:32:47 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+include('config.php');
+include('./includes/header.php');
+?>
+
+<link href="css/contact.css" type="text/css" rel="stylesheet">
+<div id="wrapper">
+    <main>
+        <!-- <h1>Welcome to the Contact Page</h1> -->
+
+        <?php
+        include('includes/form.php');
+        ?>
+
+    </main>
+
+    <aside>
+
+    <h3>This is my aside</h3>
+    </aside>
+
+<!-- end wrapper -->
+</div>
+
+<?php include('./includes/footer.php'); ?>
